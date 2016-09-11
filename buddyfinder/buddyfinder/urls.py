8c5a5adddbfc9c7993f790d5585cc98a6c134449
alalamin25 <mdabdullahalalaminp@gmail.com>
2016-09-11 22:54:18 +0600

    First unit test with url and dummy view home_page

--- buddyfinder/buddyfinder/urls.py
+++ buddyfinder/buddyfinder/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from lists import views
+
 urlpatterns = [
     url(r'^polls/', include('polls.urls',  namespace="polls")),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name = "home"),
 ]
